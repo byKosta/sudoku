7498cdb6640f58e87394695f45ce251d0730ebf9
@byKosta <rezerfpo4ta@mail.ru>
2023-05-07 19:24:37 +0200

    07.05

--- cypress/e2e/timer.cy.ts
+++ cypress/e2e/timer.cy.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress"/>
 describe('timer', () => {
     it('shows 1 seconds',  () => {
-      cy.visit('https://sudoku-raravi.vercel.app/')
+      cy.visit('/')
       for(let k = 0; k < 10; k++){
       cy.contains('.status__time', `00:0${k}`)
       }
