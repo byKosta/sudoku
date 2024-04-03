b3e702f30aa916a6a12162692a9e4475fd5acf61
@byKosta <rezerfpo4ta@mail.ru>
2024-04-03 21:39:13 +0200

    Co-authored-by: 165950756+AstroQA@users.noreply.github.com.

--- cypress/e2e/hint.cy.ts
+++ cypress/e2e/hint.cy.ts
@@ -1,14 +1,17 @@
-describe('hint', () => {
-    it('fills each empty cell',  () => {
-      cy.visit('/')
-      cy.get('.game__cell.game__cell--filled').should('have.length',45)
-      cy.get('.game__cell') .not('.game__cell--filled') .each($cell => {
-      cy.wrap($cell) .click()
-      cy.get('.status__action-hint') .click()
-      })
+/// <reference types = "Cypress"/>
 
-      cy.contains('.overlay__text', 'You') .should('be.visible');
-      cy.contains('.overlay__textspan1', 'solved') .should('be.visible')
-      cy.contains('.overlay__textspan2', 'it!') .should('be.visible')
-})
-  })
+describe('hint', () => {
+        it('fills each empty cell',  () => {
+          cy.visit('/')
+          cy.get('.game__cell').not('.game__cell--filled').each($cell => {
+          cy.wrap($cell).click()
+          cy.get('.status__action-hint').click()
+          });
+
+          cy.contains('.overlay__text', 'You').should('be.visible');
+          cy.contains('.overlay__textspan1', 'solved').should('be.visible')
+          cy.contains('.overlay__textspan2', 'it!').should('be.visible')
+          
+    });
+
+  });
